63ea7bd21da8204f7fe5ab9caaf54028cf7cb40c
David Robillard <d@drobilla.net>
2020-11-27 17:58:03 +0100

    Add missing whitespace

--- src/PatchageModule.hpp
+++ src/PatchageModule.hpp
@@ -1,5 +1,5 @@
 /* This file is part of Patchage.
- * Copyright 2007-2014 David Robillard <http://drobilla.net>
+ * Copyright 2007-2020 David Robillard <d@drobilla.net>
  *
  * Patchage is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free
@@ -40,6 +40,7 @@ public:
 	               ModuleType         type,
 	               double             x = 0,
 	               double             y = 0);
+
 	~PatchageModule();
 
 	void split();
