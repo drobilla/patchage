f51ad87e28f7580f674569cd9a9345c50d50c935
David Robillard <d@drobilla.net>
2022-09-08 15:46:48 -0400

    Use 0BSD for trivial "public domain intent" things
    Fedora takes issue with CC0.  Although it doesn't really matter for this
    stuff (dual licensed anyway, questionably "software", certainly not patentable,
    and so on), this is simpler and more consistent with the ISC license used for
    the actual software anyway.

--- osx/bundleify.sh
+++ osx/bundleify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copyright 2011-2017 David Robillard <d@drobilla.net>
-# SPDX-License-Identifier: CC0-1.0 OR GPL-3.0-or-later
+# SPDX-License-Identifier: 0BSD OR GPL-3.0-or-later
 
 if [ "$#" != 3 ]; then
 	echo "USAGE: $0 LIB_PREFIX BUNDLE EXE";
