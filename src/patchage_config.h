c5f453bfb3de0dd62676ebe8e06e73e24ab7d066
David Robillard <d@drobilla.net>
2022-09-09 21:03:50 -0400

    Patchage 1.0.10

--- src/patchage_config.h
+++ src/patchage_config.h
@@ -27,7 +27,7 @@
 #define PATCHAGE_CONFIG_H
 
 // Define version unconditionally so a warning will catch a mismatch
-#define PATCHAGE_VERSION "1.0.9"
+#define PATCHAGE_VERSION "1.0.10"
 
 #if !defined(PATCHAGE_NO_DEFAULT_CONFIG)
 
